4c0874d0e5916af0c386593a3f53d8561081e316
mrsimonmorris <simonsos@hotmail.co.uk>
2014-08-06 21:56:30 +0100

    test button

--- src/uk/co/mojaworks/normantest/screens/mainmenu/MainMenu.hx
+++ src/uk/co/mojaworks/normantest/screens/mainmenu/MainMenu.hx
@@ -5,6 +5,7 @@ import uk.co.mojaworks.norman.components.director.View;
 import uk.co.mojaworks.norman.components.display.Fill;
 import uk.co.mojaworks.norman.components.display.Image;
 import uk.co.mojaworks.norman.components.input.TouchListener;
+import uk.co.mojaworks.norman.components.interactive.Button;
 import uk.co.mojaworks.norman.components.Prefab;
 import uk.co.mojaworks.norman.core.Core;
 import uk.co.mojaworks.norman.core.GameObject;
@@ -29,20 +30,27 @@ class MainMenu extends Prefab
 		
 		// Set up the menu object here - should have access to privates as in same class
 		gameObject.add( menu );
-		gameObject.add( new Fill( 0xFF0000, 1, 1000, 600 ) );
-		gameObject.display.clipRect = new Rectangle( 50, 50, 150, 150 );
+		gameObject.add( new Fill( 0x0000FF, 1, 1000, 600 ) );
+		//gameObject.display.clipRect = new Rectangle( 50, 50, 150, 150 );
 		gameObject.transform.x = 200;
 		gameObject.transform.y = 200;
 		gameObject.transform.rotation = 0.5;
 		gameObject.add( new TouchListener() );		
 		
-		menu.child = new GameObject().add( new Image( "img/zombie.png" ) );
-		menu.child.add( new TouchListener() );
-		menu.child.display.clipRect = new Rectangle( 100, 100, 50, 50 );
-		gameObject.addChild( menu.child );
+		//menu.child = new GameObject().add( new Image( "img/zombie.png" ) );
+		//menu.child.add( new TouchListener() );
+		//menu.child.display.clipRect = new Rectangle( 100, 100, 50, 50 );
+		//gameObject.addChild( menu.child );
+		
+		var button_obj : GameObject = new GameObject();
+		var button : Button = new Button();
+		button.setup( new Fill( 0xFF0000, 1, 100, 100 ), new Fill( 0x00FF00, 1, 100, 100 ) );
+		button_obj.add( button );
+		gameObject.addChild( button_obj );
 		
 		
-		trace( menu.child.findAncestorThatHas(View) );
+		
+		//trace( menu.child.findAncestorThatHas(View) );
 	}
 	
 }
---
+++ src/uk/co/mojaworks/normantest/screens/mainmenu/MainMenuController.hx
@@ -0,0 +1,18 @@
+package uk.co.mojaworks.normantest.screens.mainmenu;
+
+import uk.co.mojaworks.norman.core.Component;
+
+/**
+ * ...
+ * @author Simon
+ */
+class MainMenuController extends Component
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
