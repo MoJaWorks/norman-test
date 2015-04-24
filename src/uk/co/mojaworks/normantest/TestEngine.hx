0d3794941bd22630c1c8125279ad7adef7f4ba7d
unknown <r1guser@PC15.R1G.local>
2015-04-24 14:05:24 +0100

    start again

--- src/uk/co/mojaworks/normantest/TestEngine.hx
+++ src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -1,9 +1,5 @@
 package uk.co.mojaworks.normantest;
-
-import uk.co.mojaworks.norman.components.director.Director;
-import uk.co.mojaworks.norman.core.GameObject;
-import uk.co.mojaworks.norman.engine.NormanApp;
-import uk.co.mojaworks.normantest.screens.mainmenu.MainMenu;
+import uk.co.mojaworks.norman.NormanApp;
 
 /**
  * ...
@@ -11,26 +7,31 @@ import uk.co.mojaworks.normantest.screens.mainmenu.MainMenu;
  */
 class TestEngine extends NormanApp
 {
-	var screen : GameObject;
-
-	public function new(stage) 
-	{
-		super( stage, 1000, 600 );
-		
-	}
-		
-	override function onStartupComplete():Void 
-	{
-		super.onStartupComplete();
-		
-		screen = new GameObject().add( new MainMenu() );
-		root.get(Director).moveToView( screen );
-		
+	
+	public function new() {
+		super();
 	}
 	
-	override public function onUpdate(seconds:Float):Void 
-	{
-		super.onUpdate(seconds);
-	}
+	//var screen : GameObject;
+//
+	//public function new(stage) 
+	//{
+		//super( stage, 1000, 600 );
+		//
+	//}
+		//
+	//override function onStartupComplete():Void 
+	//{
+		//super.onStartupComplete();
+		//
+		//screen = new GameObject().add( new MainMenu() );
+		//root.get(Director).moveToView( screen );
+		//
+	//}
+	//
+	//override public function onUpdate(seconds:Float):Void 
+	//{
+		//super.onUpdate(seconds);
+	//}
 	
 }
