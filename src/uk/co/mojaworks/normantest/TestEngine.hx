10d3b39e95f27fef1fdc3c6842bfa4998067a4a6
mrsimonmorris <simonsos@hotmail.co.uk>
2015-05-07 22:30:11 +0100

    reset

--- src/uk/co/mojaworks/normantest/TestEngine.hx
+++ src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -17,27 +17,5 @@ class TestEngine extends NormanApp
 		
 		super( config );
 	}
-	
-	//var screen : GameObject;
-//
-	//public function new(stage) 
-	//{
-		//super( stage, 1000, 600 );
-		//
-	//}
-		//
-	//override function onStartupComplete():Void 
-	//{
-		//super.onStartupComplete();
-		//
-		//screen = new GameObject().add( new MainMenu() );
-		//root.get(Director).moveToView( screen );
-		//
-	//}
-	//
-	//override public function onUpdate(seconds:Float):Void 
-	//{
-		//super.onUpdate(seconds);
-	//}
-	
+		
 }
