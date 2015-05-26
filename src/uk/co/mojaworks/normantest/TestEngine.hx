85200abdf8cb2ef5661c687d4a97c48a08a606cd
unknown <smorris@PC15.R1G.local>
2015-05-26 13:27:54 +0100

    Test render texture alpha

--- src/uk/co/mojaworks/normantest/TestEngine.hx
+++ src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -1,8 +1,11 @@
 package uk.co.mojaworks.normantest;
+import motion.Actuate;
+import motion.easing.Linear;
 import uk.co.mojaworks.norman.data.NormanConfigData;
 import uk.co.mojaworks.norman.display.FillSprite;
 import uk.co.mojaworks.norman.display.ImageSprite;
 import uk.co.mojaworks.norman.display.RenderSprite;
+import uk.co.mojaworks.norman.display.Sprite;
 import uk.co.mojaworks.norman.NormanApp;
 import uk.co.mojaworks.norman.systems.Systems;
 import uk.co.mojaworks.norman.utils.Color;
@@ -29,37 +32,28 @@ class TestEngine extends NormanApp
 		
 		var b : FillSprite = new FillSprite( Color.RED, Systems.viewport.stageWidth, Systems.viewport.stageHeight );
 		Systems.view.root.addChild( b );
-		trace("b = ", b.id );
 		
-		var o : ImageSprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png"), "barrel.png" );
-		o.color.a = 0.5;
+		var o : Sprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png"), "barrel.png" );
+		o.alpha = 0;
 		o.transform.x = 300;
 		o.transform.y = 20;
 		o.transform.scaleX = 2;
 		o.transform.scaleY = 2;
-		//f.transform.rotationDegrees = 45;
 		Systems.view.root.addChild( o );
-		trace("o = ", o.id );
 		
 		var s : RenderSprite = new RenderSprite( 200, 200 );
-		//s.alpha = 0.5;
-		s.transform.x = 0;
-		s.transform.y = 0;
-		//s.transform.rotationDegrees = 45;
 		Systems.view.root.addChild( s );
-		trace("s = ", s.id );
 		
 		var f : ImageSprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png"), "barrel.png" );
-		//f.color.a = 0.5;
-		//f.color.r = 0;
-		f.alpha = 0.5;
+		f.alpha = 0;
 		f.transform.x = 20;
 		f.transform.y = 20;
 		f.transform.scaleX = 2;
 		f.transform.scaleY = 2;
-		//f.transform.rotationDegrees = 45;
 		s.addChild( f );
-		trace("f = ", f.id );
+		
+		Actuate.tween( f, 3, { alpha: 1 } ).repeat( 200 ).ease( Linear.easeNone );
+		Actuate.tween( o, 3, { alpha: 1 } ).repeat( 200 ).ease( Linear.easeNone );
 		
 	}
 		
