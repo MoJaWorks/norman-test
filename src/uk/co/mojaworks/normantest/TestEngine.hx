cd976741e00d599020f432a2e1c82bc49a67de59
unknown <smorris@PC15.R1G.local>
2015-09-08 13:43:41 +0100

    Test masks

--- src/uk/co/mojaworks/normantest/TestEngine.hx
+++ src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -5,6 +5,7 @@ import uk.co.mojaworks.norman.data.NormanConfigData;
 import uk.co.mojaworks.norman.debug.FPS;
 import uk.co.mojaworks.norman.display.FillSprite;
 import uk.co.mojaworks.norman.display.ImageSprite;
+import uk.co.mojaworks.norman.display.MaskedSprite;
 import uk.co.mojaworks.norman.display.RenderSprite;
 import uk.co.mojaworks.norman.display.Sprite;
 import uk.co.mojaworks.norman.display.TextSprite;
@@ -19,8 +20,6 @@ import uk.co.mojaworks.norman.utils.FontUtils;
  */
 class TestEngine extends NormanApp
 {
-	var fps:FPS;
-	
 	public function new() {
 		
 		var config : NormanConfigData = new NormanConfigData();
@@ -33,42 +32,21 @@ class TestEngine extends NormanApp
 	override function onStartupComplete() 
 	{
 		super.onStartupComplete();
-		
-		var b : FillSprite = new FillSprite( Color.BLUE, Systems.viewport.stageWidth, Systems.viewport.stageHeight );
-		Systems.view.root.addChild( b );
-		
-		var r : Sprite = Systems.view.root;
-		
-		//var o : Sprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png"), "barrel.png" );
-		//o.alpha = 0;
-		//o.transform.x = 300;
-		//o.transform.y = 20;
-		//o.transform.scaleX = 2;
-		//o.transform.scaleY = 2;
-		//r.addChild( o );
-		
-		//var s : RenderSprite = new RenderSprite( 300, 300 );
-		//r.addChild( s );
-		//
-		//var g : ImageSprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png" ), "zhead.png" );
-		//s.addChild( g );
 				
-		fps = new FPS();
-		r.addChild( fps );
+		var m : MaskedSprite = new MaskedSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png" ), "zhead.png" );
+		m.x = 50;
+		Systems.view.root.addChild( m );
+
 		
+		var b : ImageSprite = new ImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png" ), "barrel.png" );
+		m.addChild( b );
 		
-		
-		
-		//Actuate.tween( f, 3, { alpha: 1 } ).repeat( 200 ).ease( Linear.easeNone );
-		//Actuate.tween( o, 3, { alpha: 1 } ).repeat( 200 ).ease( Linear.easeNone );
-		
+				
 	}
 	
 	override public function updateApp(seconds:Float):Void 
 	{
 		super.updateApp(seconds);
-		
-		fps.update( seconds );
 	}
 		
 }
