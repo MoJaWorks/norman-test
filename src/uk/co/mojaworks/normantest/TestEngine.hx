3ce515e2fbdd4a73bb37b85c0936ef4b5d1899a3
unknown <smorris@PC15.R1G.local>
2015-11-13 10:17:49 +0000

    Test text

--- src/uk/co/mojaworks/normantest/TestEngine.hx
+++ src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -1,15 +1,16 @@
 package uk.co.mojaworks.normantest;
-import uk.co.mojaworks.norman.components.delegates.SimpleButtonUIDelegate;
-import uk.co.mojaworks.norman.components.renderer.ShapeRenderer.FillShape;
-import uk.co.mojaworks.norman.components.Transform;
+import lime.graphics.console.TextureFormat;
+import uk.co.mojaworks.norman.components.renderer.TextRenderer.TextAlign;
+import uk.co.mojaworks.norman.components.renderer.TextRenderer.TextFormat;
+import uk.co.mojaworks.norman.components.text.TextInput;
+import uk.co.mojaworks.norman.components.text.TextInputKeyboardDelegate;
+import uk.co.mojaworks.norman.components.text.TextInputUIDelegate;
 import uk.co.mojaworks.norman.data.NormanConfigData;
-import uk.co.mojaworks.norman.display.MaskedSprite;
 import uk.co.mojaworks.norman.factory.GameObject;
 import uk.co.mojaworks.norman.factory.SpriteFactory;
-import uk.co.mojaworks.norman.factory.UIFactory;
 import uk.co.mojaworks.norman.NormanApp;
 import uk.co.mojaworks.norman.systems.Systems;
-import uk.co.mojaworks.norman.systems.ui.MouseEvent;
+import uk.co.mojaworks.norman.text.BitmapFont;
 import uk.co.mojaworks.norman.utils.Color;
 import uk.co.mojaworks.norman.utils.FontUtils;
 
@@ -32,27 +33,13 @@ class TestEngine extends NormanApp
 	{
 		super.onStartupComplete();
 		
-		var mask : GameObject = SpriteFactory.createMask( Systems.renderer.createTextureFromAsset("img/zombie.png"), "barrel.png" );
-		Systems.director.rootObject.transform.addChild( mask.transform );
-		
-		var image : GameObject = SpriteFactory.createImageSprite( Systems.renderer.createTextureFromAsset( "img/zombie.png" ), "zhead.png" );
-		Systems.director.rootObject.transform.addChild( image.transform );
-			
-		var fill : GameObject = SpriteFactory.createFilledSprite( Color.BLUE, 200, 200, FillShape.Ellipse );
-		mask.transform.addChild( fill.transform );
-		
-		var render : GameObject = SpriteFactory.createRenderTexture( 200, 200 );
-		render.transform.scale = 2;
-		fill.transform.addChild( render.transform );
-		
-		var text : GameObject = SpriteFactory.createTextSprite( "Hello", FontUtils.createFontFromAsset("default/arial.fnt") );
-		render.transform.addChild( text.transform );
-		
-		var delegate : SimpleButtonUIDelegate = new SimpleButtonUIDelegate();
-		delegate.enabled = true;
-		var button : GameObject = UIFactory.createImageButton( delegate, Systems.renderer.createTextureFromAsset("img/zombie.png"), "barrel.png" );
-		button.transform.y = 300;
-		Systems.director.rootObject.transform.addChild( button.transform );
+		var font : BitmapFont = FontUtils.createFontFromAsset( "default/arial.fnt" );
+		var text : GameObject = SpriteFactory.createTextSprite( "Hello", new TextFormat( font, 30 ), "text" );
+		text.renderer.color = Color.WHITE;
+		text.addComponent( new TextInputKeyboardDelegate() );
+		text.addComponent( new TextInputUIDelegate() );
+		text.addComponent( new TextInput() );
+		Systems.director.rootObject.transform.addChild( text.transform );
 		
 	}
 	
