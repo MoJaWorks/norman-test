4c2810aea2a3687a839870b5c5f48bc8193829d7
unknown <smorris@PC15.R1G.local>
2016-06-28 20:15:56 +0100

    Audio on demand

--- Sandbox/src/uk/co/mojaworks/normantest/TestEngine.hx
+++ Sandbox/src/uk/co/mojaworks/normantest/TestEngine.hx
@@ -2,9 +2,11 @@ package uk.co.mojaworks.normantest;
 import geoff.App;
 import geoff.assets.Assets;
 import uk.co.mojaworks.norman.NormanApp;
+import uk.co.mojaworks.norman.components.ui.UIDelegate;
 import uk.co.mojaworks.norman.data.NormanConfigData;
 import uk.co.mojaworks.norman.factory.GameObject;
 import uk.co.mojaworks.norman.factory.SpriteFactory;
+import uk.co.mojaworks.norman.systems.ui.PointerEvent;
 
 /**
  * ...
@@ -26,19 +28,25 @@ class TestEngine extends NormanApp
 	{
 		super.onStartupComplete();
 		
+		App.current.platform.audio.loadAsset( "scream", Assets.getPath( "audio/test.ogg" ) );
 		App.current.platform.audio.loadAsset( "loop", Assets.getPath( "audio/loop.ogg" ) );
 		App.current.platform.audio.playLooping( "loop" );
 		
-		App.current.platform.audio.loadAsset( "scream", Assets.getPath( "audio/test.ogg" ) );
-		App.current.platform.audio.playLooping( "scream" );
-		
 		var image2 : GameObject = SpriteFactory.createImageSpriteFromAsset( Assets.getPath( "img/BlueBtn.png" ) );
+		var uiListener : UIDelegate = cast image2.add( new UIDelegate() );
+		uiListener.clicked.add( onImageClicked );
 		image2.transform.y = 700;
+		
 		core.view.root.transform.addChild( image2.transform );
 		
 		var image : GameObject = SpriteFactory.createImageSpriteFromAsset( Assets.getPath( "img/zombie.png::barrel.png" ) );
 		core.view.root.transform.addChild( image.transform );
 		
+	}
+	
+	function onImageClicked( e : PointerEvent ) 
+	{
+		App.current.platform.audio.playOneShot( "scream" );
 	}
 	
 	
